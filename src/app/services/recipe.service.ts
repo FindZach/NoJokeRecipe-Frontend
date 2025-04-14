f0fed45c0cef6bb18dc1a612613b09b42ae4c00d
Zach <zach@findzach.com>
2025-04-14 08:51:52 -0400

    added pagination to recipe service

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {Recipe} from '../models/recipe/recipe.models';
-import {environment} from '../../environments/environment';
+import { Recipe } from '../models/recipe/recipe.models';
+
+export interface PaginatedResponse<T> {
+  items: T[];
+  totalItems: number;
+  page: number;
+  totalPages: number;
+  itemsPerPage: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
-  private apiUrl = environment.apiUrl + '/recipes';
+  private apiUrl = typeof process !== 'undefined' && process.env['API_URL']
+    ? `${process.env['API_URL']}/recipes`
+    : 'https://backend.nojokerecipes.com/recipes';
+
   constructor(private http: HttpClient) {}
 
   getRecipe(slug: string): Observable<Recipe> {
@@ -18,5 +28,31 @@ export class RecipeService {
 
   getAllRecipes(): Observable<Recipe[]> {
     return this.http.get<Recipe[]>(this.apiUrl);
+  }
+
+  searchRecipes(
+    query: string = '',
+    page: number = 1,
+    limit: number = 12,
+    sortBy: string = 'newest'
+  ): Observable<PaginatedResponse<Recipe>> {
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('limit', limit.toString())
+      .set('sort', sortBy);
+
+    if (query && query.trim()) {
+      params = params.set('q', query.trim());
+    }
+
+    return this.http.get<PaginatedResponse<Recipe>>(`${this.apiUrl}/search`, { params });
+  }
+
+  getFavoriteRecipes(): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>(`${this.apiUrl}/favorites`);
+  }
+
+  toggleFavorite(recipeId: string): Observable<{ isFavorite: boolean }> {
+    return this.http.post<{ isFavorite: boolean }>(`${this.apiUrl}/${recipeId}/favorite`, {});
   }
 }
