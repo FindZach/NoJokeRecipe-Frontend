5421645aa4cb9c6e72ccff427a2f29a07e4be362
Zach <zach@findzach.com>
2025-04-13 21:08:10 -0400

    Added Prerendering for recipes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,4 +1,7 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import {inject} from '@angular/core';
+import {RecipeService} from './services/recipe.service';
+import {firstValueFrom} from 'rxjs';
 
 export const serverRoutes: ServerRoute[] = [
   {
@@ -7,7 +10,12 @@ export const serverRoutes: ServerRoute[] = [
   },
   {
     path: 'recipes/:slug',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const recipeService = inject(RecipeService);
+      const recipes = await firstValueFrom(recipeService.getAllRecipes());
+      return recipes.map(recipe => ({ slug: recipe.slug }));
+    }
   },
   {
     path: '**',
