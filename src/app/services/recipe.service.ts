0fc9c4bcf4f8eaae5e78c9b1fc4ee6b6873f4079
Zach <zach@findzach.com>
2025-04-13 21:59:27 -0400

    Added dockerfile pt 3

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -9,7 +9,7 @@ import {Recipe} from '../models/recipe/recipe.models';
 export class RecipeService {
   private apiUrl = typeof process !== 'undefined' && process.env['API_URL']
     ? `${process.env['API_URL']}/recipes`
-    : 'http://localhost:8080/recipes';
+    : 'https://nojokerecipes.com/recipes';
 
   constructor(private http: HttpClient) {}
 
